d958bbff63b74acd9fbe762864074ec5444a74b0
AiurRefugee <2298641141@qq.com>
2023-07-07 22:41:38 +0800

    login

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,12 @@ import './style/common.css'
 import './style/el-style.css'
 import "./style/el-variables.scss";
 
+import Varlet from '@varlet/ui'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import '@varlet/ui/es/style'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 
@@ -13,5 +15,5 @@ const app = createApp(App)
 
 app.use(router)
 
-app.use(ElementPlus)
+app.use(Varlet)
 app.mount('#app')
